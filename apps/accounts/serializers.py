9038162a36a0c5e92b207994aa35f6bf77702ad7
Salvatore Conte <salvatoreconte@MBPdiSalvatore.homenet.telecomitalia.it>
2025-11-08 22:45:22 +0100

    Implementazione completa app accounts con JWT (signup, login, me)

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name"]
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "first_name", "last_name"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)   # hash sicuro
+        user.save()
+        return user
